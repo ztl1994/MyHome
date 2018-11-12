80d7fb9729e386504bdbc0db6ec8e169c7bdcb4e
zhangtianlong <zhangtianlong@agree.com.cn>
2018-11-12 18:00:02 +0800

    脚手架搭建

---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    测试
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {
+    async translate() {
+      if (window.File && window.FileReader && window.FileList && window.Blob) {
+        // Great success! All the File APIs are supported.
+      } else {
+        alert("The File APIs are not fully supported in this browser.");
+      }s
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
